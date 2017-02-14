acbe73bc3d72ea2b9ede1069a0d3989bc90840bc
Caue Polimanti <caue.polimanti@gmail.com>
2017-02-14 15:46:46 -0200

    Using RethinkDB to test with a real DB

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var db = require("secondthought");
+var assert = require("assert");
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -10,6 +13,17 @@ module.exports = function(grunt) {
             tasks: ['jshint']
         }
     });
+
+    grunt.registerTask("installDb", function() {
+        var done = this.async();
+        db.connect({db: "membership"}, function(err,db){
+            db.install(['users', 'logs', 'sessions'], function(err, tableResult){
+                assert.ok(err === null, err);
+                console.log("DB Installed: " + tableResult);
+                done();
+            })
+        })
+    })
     
     // loading jshint task of the instaled module 
     grunt.loadNpmTasks("grunt-contrib-jshint");
